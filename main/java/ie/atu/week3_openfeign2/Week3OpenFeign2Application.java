1a138886b1ebaf06e8c84bcb4ebf882d8990689a
G00389482@atu.ie <G00389482@atu.ie>
2024-10-02 12:26:39 +0100

    add project to github

---
+++ main/java/ie/atu/week3_openfeign2/Week3OpenFeign2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week3_openfeign2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3OpenFeign2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week3OpenFeign2Application.class, args);
+	}
+
+}
---
+++ test/java/ie/atu/week3_openfeign2/Week3OpenFeign2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3_openfeign2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3OpenFeign2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
